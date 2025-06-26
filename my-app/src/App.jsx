60f79ff55f7e3074b0847cfa2e5881048731d369
Woojiyeong <a01086824402@gmail.com>
2025-06-26 09:21:31 +0900

    React 투표 컴포넌트 만들기 과제

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Vote from './components/Vote'
+
+function App() {
+    return (
+        <>
+            <Vote />
+            <Vote />
+        </>
+    )
+}
+
+export default App
---
+++ my-app/src/components/Vote.jsx
@@ -0,0 +1,11 @@
+import "../css/Vote.css"
+
+function Vote() {
+    return (
+        <div className="container">
+            <h3>우아우ㅏㅇ</h3>
+        </div>
+    )
+}
+
+export default Vote
